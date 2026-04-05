3554110d858520384bc0126d07d5f7813ec7936b
rdmehrotra <ROHANMEHROTRA2006@GMAIL.COM>
2026-04-04 23:50:21 -0400

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -12,7 +12,7 @@ def set_bg(image_file):
     .stApp {{
         background-image: url("data:image/jpeg;base64,{data}");
         background-size: cover;
-        background-position: 50% center;
+        background-position: 10% center;
         background-repeat: no-repeat;
         background-attachment: fixed;
     }}
